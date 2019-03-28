583c2a6c8484591877d690e3574c97f0a72d7d19
Nathaliacr <46941173+Nathaliacr@users.noreply.github.com>
2019-03-28 07:51:40 -0500

    Add files via upload

---
+++ Complejo.py
@@ -0,0 +1,28 @@
+import unittest
+import complejo
+import math
+import numpy as np
+
+class Complejo():
+    
+    def __init__(self, real, imaginario):
+
+        self.real = real
+        self.imaginario = imaginario
+        self.norma = np.sqrt(self.imaginario**2+self.real**2)
+        
+        
+    def conjugado(self):
+        
+        self.imaginario = -self.imaginario
+    
+    def calcula_norma(self):
+        
+        self.norma = np.sqrt(self.imaginario**2+self.real**2)
+        
+    def pow(self, n):
+        
+        return self.imaginario**n
+    
+        
+        
